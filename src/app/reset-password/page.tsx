1fd02283dc91d4ef062f5ae2a51359f5d3d9b808
iiioooiso <ashutoshrajput8642@gmail.com>
2025-08-19 14:25:14 +0530

    modified:   src/app/reset-password/page.tsx

--- src/app/reset-password/page.tsx
+++ src/app/reset-password/page.tsx
@@ -2,7 +2,7 @@ export const dynamic = "force-dynamic";
 
 import ResetPassword from "./ResetPassword";
 
-export default function Page({ searchParams }: { searchParams: { code?: string } }) {
+export default async function Page({ searchParams }: { searchParams: { code?: string } }) {
     const code = searchParams?.code ?? null;
     return <ResetPassword code={code} />;
 }
